bd8d7d97e976ef9ae7a1015f852acc2e7c4bc5f9
Vinicius Renan Petter <71847367+ViniciusPetter@users.noreply.github.com>
2025-04-20 12:12:28 -0300

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Easter Egg Collector-0.1.0";
+const contentToCache = [
+    "Build/Easter Egg Collector.loader.js",
+    "Build/Easter Egg Collector.framework.js.unityweb",
+    "Build/Easter Egg Collector.data.unityweb",
+    "Build/Easter Egg Collector.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
